c28416ee025311afa682b94c0e49d064e3f772fa
Bharath varma <99080776+Moderli@users.noreply.github.com>
2025-05-10 12:27:12 +0530

    Update data.js

--- src/app/data.js
+++ src/app/data.js
@@ -21,7 +21,7 @@ export const projectsData = [
     id: 1,
     name: "Project clay",
     description: "Traditional college counselling is out of touch and expensive. Learning new skills is hard. We pair you with an elder sibling who will guide you through it.",
-    date: "4-2-2025",
+    date: "2-4-2025",
     demoLink: "https://drive.google.com/drive/folders/1A1QiJICIQ2ETamkFVbZhpC0UFh2YRGU6",
     logo: "/logos/images.jpeg"
   },
@@ -37,7 +37,7 @@ export const projectsData = [
     id: 3,
     name: "Podcast circle",
     description: "A podcast platformWhile doing affiliate marketing, I learned video editing to create better content. I started posting on Instagram at night, sharing value around sales and lead gen. As my page grew, inbound leads started coming in—and that’s how freelancing began.",
-    date: "5-4-2025",
+    date: "4-5-2025",
     demoLink: "https://drive.google.com/drive/folders/1ANpxofeqJeFh5rhPRvk_FwBiuyYGR_M2",
     logo: "/logos/podcastcircle.jpeg"
   },
@@ -45,7 +45,7 @@ export const projectsData = [
     id: 4,
     name: "Master's union",
     description: "During my internship at Masters’ Union, I worked on promoting various competitions and programs. The biggest lesson I learned? Marketing is all about ROI—no matter how creative your work is, if it doesn’t bring results, it doesn’t matter. It was an amazing experience working on real projects, leading a team, and pushing myself. I even topped the leaderboards, which made it all the more rewarding.",
-    date: "15-3-2025",
+    date: "3-15-2025",
     demoLink: "https://drive.google.com/drive/folders/1A0ECYZ07wX_tAETWzTXN0U6Qcheg4qxp",
     logo: "/logos/masterunion.jpeg"
   },
@@ -53,7 +53,7 @@ export const projectsData = [
     id: 5,
     name: "HD Crafts",
     description: "Since childhood, I’ve been passionate about art and craft—sketching pencil portraits and creating personalized handmade items. This love for creativity led me to start my own craft business through a simple Instagram page and local groups. I successfully completed 50+ customized orders across my area and the state of Punjab. It was my first real taste of entrepreneurship.",
-    date: "12-2-2020",
+    date: "2-12-2020",
     demoLink: "https://drive.google.com/drive/folders/1AjhxNWwdXGEs32gsvlep88lbjMYEk6J5",
     logo: "/logos/HD.jpeg"
   },
@@ -61,7 +61,7 @@ export const projectsData = [
     id: 6,
     name: "HustleShala",
     description: "It all started when I had this idea and teamed up with three other teens I met online—we were just four online friends trying to build something meaningful. That’s how Hustle Shala was born. We organized free career counselling workshops, ran skill-based competitions, and even created challenges where students had to team up and make real money by completing specific tasks within a set time. It was messy, exciting, and insanely hectic—late nights, 1000s of creatives, reels, PDFs, and back-to-back events, all managed by us. No mentors, no funding—just pure hustle. It was my first real experience leading a team and running actual operations. Even though we shut it down with minimal profit, the lessons it gave me in execution, teamwork, and leadership were priceless.",
-    date: "22-06-2024",
+    date: "06-22-2024",
     demoLink: "https://drive.google.com/drive/folders/1AbiFkuZ_6M_YOdES8RRj3sB1K5dwwV_p",
     logo: "/logos/hs.jpeg"
   },
@@ -69,7 +69,7 @@ export const projectsData = [
     id: 7,
     name: "Freelancing",
     description: "While doing affiliate marketing, I learned video editing to create better content. I started posting on Instagram at night, sharing value around sales and lead gen. As my page grew, inbound leads started coming in—and that’s how freelancing began.",
-    date: "08-01-2024",
+    date: "01-08-2024",
     demoLink: "https://drive.google.com/drive/folders/1Aphlm1IUhoN5bzIoF2qZsXydrl_Q0QIu",
     logo: "/logos/freelancer.png"
   },
