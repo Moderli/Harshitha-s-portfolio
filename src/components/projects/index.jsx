a9581815f45a2046b9c6780c7e1591da94fd70e8
Bharath varma <99080776+Moderli@users.noreply.github.com>
2025-05-10 14:48:39 +0530

    Update index.jsx

--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -21,6 +21,7 @@ const ProjectList = ({ projects }) => {
       animate="show"
       className="w-full max-w-auto  xl:max-w-4xl px-4 mx-auto lg:px-16 space-y-6 md:space-y-8 flex flex-col items-center"
     >
+      <h1 className="text-2xl font-bold">Click on the project to view more</h1>
       {projects.map((project, index) => {
         return <ProjectLayout key={index} {...project} />;
       })}
